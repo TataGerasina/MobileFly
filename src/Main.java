acf175da914bd63ce2639f96992fb9016b60c667
Tatyana Gerasina <gtstata@gmail.com>
2024-01-13 13:21:43 +0300

    files added

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100; //баланс
+        int refill = 1100; // пополнение
+
+        int amount = balance + refill; // баланс счёта + пополнение
+        int bonus;
+        int i = 1000;
+        if (refill > i) {
+                bonus = refill / 100;
+                System.out.println("Количество бонусных рублей на счёту: " + bonus);
+            } else {
+                bonus = 0;
+            System.out.println("Количество бонусных рублей на счёту: " + 0);
+        }
+        int finish = amount + bonus; // Итоговая сумма счёта
+
+        System.out.println("Баланс вашего счета составляет: " + finish);
+
+            //int bonus_rub = summa ? refill/ 100 : 0;
+            //System.out.println("из них бонусных рублей: " + bonus);
+
+        }
+
+    }
